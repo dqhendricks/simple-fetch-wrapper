64c509c48ba06b5d006dcd51515fe0514baaf570
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2024-05-07 13:15:05 -0700

    Update Client.js

--- Client.js
+++ Client.js
@@ -14,6 +14,7 @@ export function request(endpoint, { body, ...customConfig } = {}) {
   }
   const config = {
     method: body ? "POST" : "GET",
+    body: body,
     ...customConfig,
     headers: {
       ...headers,
